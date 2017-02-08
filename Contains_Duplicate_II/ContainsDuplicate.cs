ac4ce359e62c41d98c675c8564631a71fb1734a8
James <jame2408@gmail.com>
2017-02-08 11:21:57 +0800

    failed test : k is zero and should return fales.

--- Contains_Duplicate_II/ContainsDuplicate.cs
+++ Contains_Duplicate_II/ContainsDuplicate.cs
@@ -2,13 +2,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Contains_Duplicate_II
-{
+{    
     [TestClass]
     public class ContainsDuplicate
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_k_is_zero_should_return_false()
         {
+            var nums = new int[] { 1, 1 };
+            var k = 0;
+
+            var action = new Solution();
+
+            Assert.IsFalse(action.ContainsNearbyDuplicate(nums, k));
         }
     }
 }
---
+++ Contains_Duplicate_II/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contains_Duplicate_II
+{
+    public class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
