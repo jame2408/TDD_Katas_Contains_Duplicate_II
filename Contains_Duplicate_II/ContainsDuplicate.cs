c0a3588b5791a38142e1b4d7e3af5902f7b41710
James <jame2408@gmail.com>
2017-02-08 11:05:07 +0800

    Initial Project

---
+++ Contains_Duplicate_II/ContainsDuplicate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contains_Duplicate_II
+{
+    [TestClass]
+    public class ContainsDuplicate
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
