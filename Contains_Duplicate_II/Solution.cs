bb022c644c68d8f4d61d593da26fa82c2fb49ef6
James <jame2408@gmail.com>
2017-02-08 11:51:09 +0800

    pass test case : nums is {1, 2} and k is 1, should return false.

--- Contains_Duplicate_II/Solution.cs
+++ Contains_Duplicate_II/Solution.cs
@@ -14,7 +14,17 @@ namespace Contains_Duplicate_II
             {
                 return false;
             }
-            return true;
+
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                map[nums[i]] = i;
+            }
+            return false;
         }
     }
 }
